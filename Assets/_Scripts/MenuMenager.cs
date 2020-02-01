b450252048708689a1cebbba11eae3238a799e28
Wacław <47414520+Som3one@users.noreply.github.com>
2020-02-01 17:05:31 +0100

    Wacus
    Scena z Arena
    Menu
    Swiatlo i post processing

---
+++ Assets/_Scripts/MenuMenager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class MenuMenager : MonoBehaviour
+{
+    public Button Play;
+    public Button Settings;
+    public Button Quit;
+    public TextMeshProUGUI TurnOff;
+    public Button Skip;
+    //public bool ustawienia;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void OnClickGame()
+    {
+        //SceneManager.LoadSceneAsync("Arena"); odkomentowac jak bedzie gotowa scena
+        Debug.Log("Grasz");
+
+    }
+
+    public void OnClickSettings()
+    {
+        Play.gameObject.SetActive(false);
+        Settings.gameObject.SetActive(false);
+        Quit.gameObject.SetActive(false);
+        TurnOff.gameObject.SetActive(false);
+        Skip.gameObject.SetActive(true);
+        Debug.Log("Ustawienia");
+        
+    }
+
+    public void OnClickQuit()
+    {
+        Application.Quit();
+        Debug.Log("Wyszedles z gry");
+
+    }
+    public void OnClickSkip()
+    {
+        Play.gameObject.SetActive(true);
+        Settings.gameObject.SetActive(true);
+        Quit.gameObject.SetActive(true);
+        TurnOff.gameObject.SetActive(true);
+        Skip.gameObject.SetActive(false);
+        Debug.Log("Skipped");
+    }
+
+}
