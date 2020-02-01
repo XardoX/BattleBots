8e78aa453c16517061feb4a9039fb3306aee0f51
Wacław <47414520+Som3one@users.noreply.github.com>
2020-02-01 23:45:48 +0100

    Arena

---
+++ Assets/_Scripts/FadeScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class FadeScript : MonoBehaviour
+{
+
+    public Animator animator;
+
+   
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            animator.SetTrigger("FadeOUT");
+            Debug.Log("Dziala");
+        }
+    }
+}
