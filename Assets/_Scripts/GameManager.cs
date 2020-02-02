2b174adfe6f28aefebff2d290893afd0234b6cf6
XardoX <jurek.gab.jg@gmail.com>
2020-02-02 15:00:42 +0100

    courutine

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -42,22 +42,29 @@ public class GameManager : MonoBehaviour
         {
             canvasAnim.SetTrigger("StartCounting");
             Debug.Log("Before");
-            yield return new WaitForSeconds(3.0f);
+            yield return new WaitForSeconds(3.1f);
             Debug.Log("after");
             SetActivePlayers(true);
             timerLeft = roundTime;
             yield return new WaitForSeconds(roundTime);
             SetActivePlayers(false);
             canvasAnim.SetTrigger("FadeOUT");
-            yield return new WaitForSeconds(1.0f);
+            yield return new WaitForSeconds(1.1f);
             separateCams.SetActive(true);
             
             SetPlayerTransform(playerOneBox, playerTwoBox);
             canvasAnim.SetTrigger("StartCounting");
-            yield return new WaitForSeconds(3.0f);
+            yield return new WaitForSeconds(3.1f);
             timerLeft = repairTime;
             SetActivePlayers(true);
+            yield return new WaitForSeconds(repairTime);
+            SetActivePlayers(false);
+            canvasAnim.SetTrigger("FadeOUT");
+            yield return new WaitForSeconds(1.1f);
+            separateCams.SetActive(false);
+            SetPlayerTransform(playerOneArena, playerTwoArena);
         }
+
     }
     void SetPlayerTransform(Transform OnePos, Transform TwoPos)
     {
